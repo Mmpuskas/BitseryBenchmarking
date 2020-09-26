c2248ece5e05f3468dc61deb62f156b155d65557
Michael Puskas <mmpuskas@gmail.com>
2020-09-26 13:46:49 -0700

    Added timing. Behaves weird in windows, but seems alright in linux.

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
 #include "bitsery/bitsery.h"
 #include "bitsery/adapter/buffer.h"
 #include "bitsery/traits/vector.h"
 #include "bitsery/traits/array.h"
+#include <iostream>
+#include <chrono>
 
 // Define our test struct.
 enum class InputState : uint8_t
@@ -52,6 +53,7 @@ using OutputAdapter = bitsery::OutputBufferAdapter<std::vector<uint8_t>>;
 using InputAdapter = bitsery::InputBufferAdapter<std::vector<uint8_t>>;
 
 int main() {
+    auto totalStart = std::chrono::high_resolution_clock::now();
     // Set up our data.
     Entity entity;
     entity.id = 42;
@@ -73,17 +75,33 @@ int main() {
 
     // Serialize the data.
     std::vector<uint8_t> sendBuffer;
+    sendBuffer.reserve(35000);
+    auto serializeStart = std::chrono::high_resolution_clock::now();
+
     std::size_t writtenSize = bitsery::quickSerialization<OutputAdapter>(sendBuffer, entityUpdate);
+
+    auto serializeEnd = std::chrono::high_resolution_clock::now();
+    auto serializeDuration = std::chrono::duration_cast<std::chrono::microseconds>(serializeEnd - serializeStart).count();
 
     // Deserialize the data.
     EntityUpdate receivedUpdate{};
-    auto result = bitsery::quickDeserialization<InputAdapter>( { sendBuffer.begin(),
-            writtenSize }, receivedUpdate);
+    auto deserializeStart = std::chrono::high_resolution_clock::now();
 
+    auto result = bitsery::quickDeserialization<InputAdapter>({ sendBuffer.begin(),
+            writtenSize}, receivedUpdate);
+
+    auto deserializeEnd = std::chrono::high_resolution_clock::now();
+    auto deserializeDuration = std::chrono::duration_cast<std::chrono::microseconds>(deserializeEnd - deserializeStart).count();
+
+    auto totalEnd = std::chrono::high_resolution_clock::now();
+    auto totalDuration = std::chrono::duration_cast<std::chrono::microseconds>(totalEnd - totalStart).count();
     // First holds the error, second holds whether the buffer was successfully read from beginning to end.
     if (result.second) {
         std::cout << "Entities: " << receivedUpdate.entities.size() << std::endl;
         std::cout << "Message size: " << writtenSize << std::endl;
+        std::cout << "Serialization time: " << serializeDuration << "us" << std::endl;
+        std::cout << "Deserialization time: " << deserializeDuration << "us" << std::endl;
+        std::cout << "Total time: " << totalDuration << "us" << std::endl;
     }
     else {
         std::cout << "Error reading: " << static_cast<unsigned int>(result.first) << std::endl;
